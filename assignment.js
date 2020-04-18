bfe3256e9bea324ca8b8cea429b7e1b065386781
priya-chatwani <priyac5@stanford.edu>
2020-04-18 12:11:48 -0700

    edited index file and cleared assignment file

---
+++ assignment.js
@@ -0,0 +1,7 @@
+console.log("Hello World! I'm printing from `assignment.js`");
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * *
+                  COVID-19 MATCHING
+* * * * * * * * * * * * * * * * * * * * * * * */
+
