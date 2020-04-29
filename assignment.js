8283097d8b365f011b2ffd7ea78abcf6c435d5f2
priya-chatwani <priyac5@stanford.edu>
2020-04-29 11:48:47 -0700

    comments for TODO

--- assignment.js
+++ assignment.js
@@ -67,7 +67,7 @@ function inputToArr(input) {
  * > int - abstracted geographical distance
  */
 function getDistance(a, b) {
-  return ((addresses[a][0] - addresses[b][0]) ** 2) + ((addresses[a][1] - addresses[b][1]) ** 2)
+  // TODO: return Euclidian distance between points a and b
 }
 
 /*
@@ -84,12 +84,30 @@ function getDistance(a, b) {
  * > boolean - True if volunteer wants to change their request, False if volunteer wants to keep match as is
  */
 function volPrefersNewReq(volunteer, matchReq, newMatchReq) {
-	// If new match request needs their pet walked and this volunteer has a pet and currently 
-	// has a match request without one, then this new match is preferred.
-	if (newMatchReq in needsPetWalked && volunteer in hasPet && !(matchReq in needsPetWalked)) {
-		return true;
-	}
-	return false;
+	// TODO: return True or False here. Return True if the volunteer has a pet and the new match request 
+	// needs their pet walked (and the current match request doesn't).
+}
+
+/*
+ * This function sorts a dictionary by its value by converting it to an array of tuples. 
+ * In JS, dictionaries cannot be sorted.
+ * ----
+ * Params:
+ * > dict - dictionary of key, value pairs
+ *
+ * Returns:
+ * > arr - An array of tuples [key, value] in ascending order of value
+ */
+function sortDictByValue(dict) {
+	// Turn the dictionary into an array of tuples [key, value] so that it can be sorted
+	var arr = Object.keys(dict).map(function(key) {
+		return [key, dict[key]];
+	});
+	// Sort the array based on the second element (dict value)
+	arr.sort(function compare(a, b) {
+	  return a[1] - b[1]
+	})	
+	return arr;
 }
 
 /*
@@ -103,22 +121,8 @@ function volPrefersNewReq(volunteer, matchReq, newMatchReq) {
  * > rankings - An array of tuples [volunteer name, distance] sorted in ascending distance
  */
 function getRankedVolunteers(matchReq, volunteerArr) {
-	// Dictionary where volunteerRankings[volunteer] = distance between matchReq and volunteer
-	var volunteerRankings = {}
-	for (var i = 0; i < volunteerArr.length; i++) {
-		volunteerRankings[volunteerArr[i]] = getDistance(matchReq, volunteerArr[i]);
-	}
-	// Dictionaries cannot be sorted in JS.
-	// Turn the dictionary into an array of tuples [volunteer name, distance] so that it can be sorted
-	var rankings = Object.keys(volunteerRankings).map(function(key) {
-		return [key, volunteerRankings[key]];
-	});
-	// Sort the array based on the second element (distance)
-	rankings.sort(function compare(a, b) {
-	  return a[1] - b[1]
-	})	
-	// Returns a list of tuples [volunteer name, distance] in ascending distance
-	return rankings;		
+	// TODO: create a dictionary to save the distance between matchReq and each volunteer 
+	// Return sortRankedVolunteers(your_dict)
 }
 
 /*
@@ -129,9 +133,8 @@ function getRankedVolunteers(matchReq, volunteerArr) {
  * > matchRequestInput - string of comma-separated match request names
  *
  * Returns:
- * > JSON.stringify(volToMatchReq) - A dictionary with volunteer names as keys and the corresponding 
- * match request names as values. The dictionary goes through JSON.stringify so that it can be 
- * rendered in the HTML file
+ * > A dictionary as follows: dict[volunteer name] = paired match request. 
+ * Use JSON.stringify(dict) to render the dict object in the HTML file
  */
 function createMatches(volunteerInput, matchRequestInput) {
 	var volunteerArr = inputToArr(volunteerInput);
@@ -140,48 +143,24 @@ function createMatches(volunteerInput, matchRequestInput) {
 	if (!isValid(volunteerArr) || !isValid(matchRequestArr) || volunteerArr.length !== matchRequestArr.length) {
 		return "Invalid inputs"
 	}
-	// First, initialize all volunteers and match requests to be free
-	// Hint: can you use an array or dictionary to track this? Mark a volunteer as "free" or use a boolean?
-	var volToMatchReq = {}
-	var matchReqToVol = {}
-	for (var i = 0; i < volunteerArr.length; i++) {
-		volToMatchReq[volunteerArr[i]] = "free";
-		matchReqToVol[matchRequestArr[i]] = "free";
-	}
 	
-	// Create a counter to track how many matches have been made
-	var numMatchesMade = 0
+	// TODO: Initialize all volunteers and match requests to be free
+	// Hint: can you use an array or dictionary to track this? Mark a volunteer as "free" or use a boolean?
+	
+	var numMatchesMade = 0 // Counter to track how many matches have been made
     // While there are unfulfilled match requests
     while (numMatchesMade < matchRequestArr.length) {
-    	// pick the first unfulfilled match request
-    	var currMatchReq;
-    	for (var i = 0; i < matchRequestArr.length; i++) {
-    		if (matchReqToVol[matchRequestArr[i]] === "free") {
-    			currMatchReq = matchRequestArr[i]
-    			break;
-    		}
-    	}
-    	// Loop through all volunteers. Remember that rankedVolunteers is an array of tuples
-    	var rankedVolunteers = getRankedVolunteers(currMatchReq, volunteerArr)
-    	for (var i = 0; i < rankedVolunteers.length && matchReqToVol[currMatchReq] === "free"; i++) {
-    		var currVolunteer = rankedVolunteers[i][0]
-    		// If this volunteer is free, assign the match request to them
-	    	if (volToMatchReq[currVolunteer] === "free") {
-	    		volToMatchReq[currVolunteer] = currMatchReq
-	    		matchReqToVol[currMatchReq] = currVolunteer
-	    		numMatchesMade += 1
-	    	} 
-	    	// If this choice is not, see who the volunteer is matched to. Check if they want to switch.
-	    	else {
-	    		var prevMatchReq = volToMatchReq[currVolunteer]
-	    		// If the volunteer prefers this new match over their old one, then redo the match
-	    		if (volPrefersNewReq(currVolunteer, prevMatchReq, currMatchReq)) {
-	    			volToMatchReq[currVolunteer] = currMatchReq
-	    			matchReqToVol[currMatchReq] = currVolunteer
-	    			matchReqToVol[prevMatchReq] = "free"
-	    		}
-	    	}
-    	}
+    	numMatchesMade += 1 // delete this line when you add your implementation
+    	// TODO: Pick the first unfulfilled match request
+    	
+    	// TODO: Loop through all volunteers in order of the current match request's rankings to find a match. 
+    	// If the volunteer is free, assign the match request to them
+    	// If the volunteer is not free, see who they are currently matched to and determine if they should switch
+    	// Hint: In your for loop, include the condition that the current match request remains free
+    	// Hint: Remember that rankedVolunteers is an array of tuples
+    	
     }
-	return JSON.stringify(volToMatchReq)
+    // TODO: Return your final mapping. If the final mapping is a dictionary, return JSON.stringify(dict) 
+    // to render the dict object in the HTML file
+    return ""
 }
