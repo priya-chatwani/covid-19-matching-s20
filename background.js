dae2abe33ac81afbd2bf66a63ed8fb919b5ae841
priya-chatwani <priyac5@stanford.edu>
2020-04-29 11:22:30 -0700

    comments

--- background.js
+++ background.js
@@ -2,23 +2,18 @@
                   COVID-19 MATCHING
 * * * * * * * * * * * * * * * * * * * * * * * */
 
-/* Handles submit buttons */
-//Match
+/* Handles Match submit button */
 function submitMatches() {
   var volunteers = document.getElementById("volunteers").value
   var matchRequests = document.getElementById("match_requests").value
   if (volunteers.length == 0 || matchRequests.length == 0) {
     return
   }
-  console.log(volunteers, matchRequests)
   var matches = createMatches(volunteers, matchRequests)
   if (matches.length == 0) {
   	return
   }
-  var display_element = document.getElementById("match_keys")
+  var display_element = document.getElementById("matches")
   display_element.style = ""
   display_element.innerHTML = matches
-  var display_element2 = document.getElementById("match_values")
-  display_element2.style = ""
-  //display_element2.innerHTML = matches.values()
 }
--- solutions.js
+++ solutions.js
@@ -18,30 +18,13 @@ var needsVirtualHelp = new Set("Abdollah")
 var needsGroceries = new Set("Henry", "Anita", "Clydean", "Sima", "Abdollah")
 
 /*
- * Returns the "distance" between two people
+ * This function returns true if the given array has valid entries 
  * ----
  * Params:
- * > a - person 1
- * > b - person 2
+ * > arr - array of volunteer or match request names
  *
  * Returns:
- * > int - abstracted geographical distance
- */
-
-function getDistance(a, b) {
-  return ((addresses[a][0] - addresses[b][0]) ** 2) + ((addresses[a][1] - addresses[b][1]) ** 2)
-}
-
-/*
- * This function returns true if the volunteer prefers
- * the given match request over their current match request 
- * ----
- * Params:
- * > volunteer - name of volunteer
- * > matchRequest - name of match request
- *
- * Returns:
- * > int - abstracted geographical distance
+ * > boolean - if valid
  */
 function isValid(arr) {
 	if (arr.length === 0) {
@@ -56,6 +39,15 @@ function isValid(arr) {
 	return true
 }
 
+/*
+ * This function modifies a string with comma separated text into an array
+ * ----
+ * Params:
+ * > input - string with comma separated names of volunteers or match requests
+ *
+ * Returns:
+ * > arr - array representation
+ */
 function inputToArr(input) {
 	// Remove spaces from the input
 	var list = input.split(" ").join(""); 
@@ -64,24 +56,33 @@ function inputToArr(input) {
 	return arr;
 }
 
-function getPreferences(matchReq, volunteers) {
-	// Needs grocery help? Rank based on location
-	if (matchReq in needsGroceries) {
-		var distanceFrom = {}
-		for (var i = 0; i < volunteers.length; i++) {
-			distanceFrom[volunteers[i]] = getDistance(matchReq, volunteers[i])
-		}
-		var rankings = Object.keys(distanceFrom).map(function(key) {
-			return [key, distanceFrom[key]];
-		});
-		// Sort the array based on the second element
-		rankings.sort(function(a, b) { return a[1] - b[1]; });
-		return rankings;
-	}
-	// Just needs virtual help? Return any ordering
-	return volunteers;
+/*
+ * Returns the "distance" between two people
+ * ----
+ * Params:
+ * > a - person 1
+ * > b - person 2
+ *
+ * Returns:
+ * > int - abstracted geographical distance
+ */
+function getDistance(a, b) {
+  return ((addresses[a][0] - addresses[b][0]) ** 2) + ((addresses[a][1] - addresses[b][1]) ** 2)
 }
 
+/*
+ * This function determines whether the volunteer prefers a new match request over their current one.
+ * For this implementation, pet walking is used as a criteria for switching. 
+ * The volunteer will switch if they have a pet and this new match request needs their pet walked.
+ * ----
+ * Params:
+ * > volunteer - name of volunteer for whom we are optimizing their match
+ * > matchReq - name of volunteer's current match request
+ * > newMatchReq - name of a different match request to whom volunteer is comparing their current match request
+ *
+ * Returns:
+ * > boolean - True if volunteer wants to change their request, False if volunteer wants to keep match as is
+ */
 function volPrefersNewReq(volunteer, matchReq, newMatchReq) {
 	// If new match request needs their pet walked and this volunteer has a pet and currently 
 	// has a match request without one, then this new match is preferred.
@@ -91,29 +92,28 @@ function volPrefersNewReq(volunteer, matchReq, newMatchReq) {
 	return false;
 }
 
-function getRankings(volunteerArr, matchRequestArr) {
-	volToMatchPreferences = {}
-	for (var i = 0; i < volunteerArr.length; i++) {
-		for (var j = 0; j < matchRequestArr.length; j++) {
-			if (j === 0) {
-				volToMatchPreferences[volunteerArr[i]] = {}
-			}
-			volToMatchPreferences[volunteerArr[i]][matchRequestArr[j]] = getDistance(matchRequestArr[j], volunteerArr[i])
-		}
-	}
-	return volToMatchPreferences;
-}
-
-function getRankedVolunteer(match, volunteerArr) {
+/*
+ * This function outputs the given match request's volunteer preferences based on distance.
+ * ----
+ * Params:
+ * > matchReq - name of match request whose rankings will be returned
+ * > volunteerArr - array of volunteers who can be matched
+ *
+ * Returns:
+ * > rankings - An array of tuples [volunteer name, distance] sorted in ascending distance
+ */
+function getRankedVolunteers(matchReq, volunteerArr) {
+	// Dictionary where volunteerRankings[volunteer] = distance between matchReq and volunteer
 	var volunteerRankings = {}
 	for (var i = 0; i < volunteerArr.length; i++) {
-		volunteerRankings[volunteerArr[i]] = getDistance(match, volunteerArr[i]);
+		volunteerRankings[volunteerArr[i]] = getDistance(matchReq, volunteerArr[i]);
 	}
+	// Dictionaries cannot be sorted in JS.
+	// Turn the dictionary into an array of tuples [volunteer name, distance] so that it can be sorted
 	var rankings = Object.keys(volunteerRankings).map(function(key) {
 		return [key, volunteerRankings[key]];
 	});
-	// var rankings = Object.keys(volunteerRankings)
-	// Sort the array based on the second element
+	// Sort the array based on the second element (distance)
 	rankings.sort(function compare(a, b) {
 	  return a[1] - b[1]
 	})	
@@ -121,6 +121,18 @@ function getRankedVolunteer(match, volunteerArr) {
 	return rankings;		
 }
 
+/*
+ * This function outputs the matches that have been created by the Stable Marriage Algorithm.
+ * ----
+ * Params:
+ * > volunteerInput - string of comma-separated volunteer names
+ * > matchRequestInput - string of comma-separated match request names
+ *
+ * Returns:
+ * > JSON.stringify(volToMatchReq) - A dictionary with volunteer names as keys and the corresponding 
+ * match request names as values. The dictionary goes through JSON.stringify so that it can be 
+ * rendered in the HTML file
+ */
 function createMatches(volunteerInput, matchRequestInput) {
 	var volunteerArr = inputToArr(volunteerInput);
 	var matchRequestArr = inputToArr(matchRequestInput);
@@ -136,9 +148,7 @@ function createMatches(volunteerInput, matchRequestInput) {
 		volToMatchReq[volunteerArr[i]] = "free";
 		matchReqToVol[matchRequestArr[i]] = "free";
 	}
-	// get the volunteers' preferences based on distance. these rankings are the same for match requests
-	var volToMatchPreferences = getRankings(volunteerArr, matchRequestArr);
-
+	
 	// Create a counter to track how many matches have been made
 	var numMatchesMade = 0
     // While there are unfulfilled match requests
@@ -151,21 +161,20 @@ function createMatches(volunteerInput, matchRequestInput) {
     			break;
     		}
     	}
-    	// Loop through all volunteers
-    	var rankedVolunteers = getRankedVolunteer(currMatchReq, volunteerArr)
+    	// Loop through all volunteers. Remember that rankedVolunteers is an array of tuples
+    	var rankedVolunteers = getRankedVolunteers(currMatchReq, volunteerArr)
     	for (var i = 0; i < rankedVolunteers.length && matchReqToVol[currMatchReq] === "free"; i++) {
     		var currVolunteer = rankedVolunteers[i][0]
-    		// Assign match request to volunteer if they are free
+    		// If this volunteer is free, assign the match request to them
 	    	if (volToMatchReq[currVolunteer] === "free") {
 	    		volToMatchReq[currVolunteer] = currMatchReq
 	    		matchReqToVol[currMatchReq] = currVolunteer
 	    		numMatchesMade += 1
 	    	} 
-	    	// this choice isn't free? See who this volunteer is matched to
+	    	// If this choice is not, see who the volunteer is matched to. Check if they want to switch.
 	    	else {
 	    		var prevMatchReq = volToMatchReq[currVolunteer]
-	    		// if the volunteer prefers this new match over their old one,
-	    		// then redo the match
+	    		// If the volunteer prefers this new match over their old one, then redo the match
 	    		if (volPrefersNewReq(currVolunteer, prevMatchReq, currMatchReq)) {
 	    			volToMatchReq[currVolunteer] = currMatchReq
 	    			matchReqToVol[currMatchReq] = currVolunteer
